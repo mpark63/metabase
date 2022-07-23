a484770c80bfb3fe2bf58611246b252d38fa1ed2
mpark63 <minspk13@gmail.com>
2022-07-23 22:17:45 +0900

    write one file per course write one question per section write demographic groups per subsection

--- demo.py
+++ demo.py
@@ -43,18 +43,13 @@ def queryForCount(qstring):
     c = c[0]
 
     return c
-    
+
+#%% QUESTIONS AND RESPONSES ==========================================================
 Qs = {
-        'would_recommend_course_to_friend_interested_in_0et0_eng' 
-            : 'Would recommend course to friend interested in engineering', 
-        # 'an_engineering_degree_will_guarantee_me_a_job_when_i_graduate' :
-        #     'An engineering_degree will guarantee me a job when I graduate',
-        # 'engineers_are_well_paid':
-        #     'Engineers are well paid',
-        # 'engineering_plays_an_important_role_in_solving_societys_problems':
-        #     'Engineering plays an important role in solving societys problems',
-        # 'i_like_to_solve_problems_and/or_figure_out_how_things_work':
-        #     'I like to solve problems and/or figure out how things work'
+        'i_intend_to_study_0et0_engineering_in_college' 
+            : 'I intend to study engineering in college', 
+        'i_feel_prepared_to_study_0et0_engineering_in_college':
+            'I feel prepared to study engineering in college',
     }
 
 respDict = {
@@ -65,62 +60,117 @@ respDict = {
             'Strongly disagree':'Strongly disagree',
             'Total': 'Total'
             }
+
 #%% DEMOGRAPHICS ==========================================================
 dictSex = {'col':'sex',
-    'data': {'M':'Male','F':'Female'}
-    }
+            'data': {'M':'Male','F':'Female'}
+            }
+dictRace = {'col':'race',
+            'data': {
+                'American Indian or Alaska Native':'American Indian or Alaska Native',
+                'Asian':'Asian',
+                'Black or African American':'Black or African American',
+                'Native Hawaiian or Other Pacific':'Native Hawaiian or Other Pacific',
+                'White':'White'}
+            }
+dictEthn= {'col':'hispanic',
+            'data': {'Y':'Hispanic','N':'Not Hispanic'}
+            }
+dictGrade = {'col':'current_grade',
+            'data': {
+                '9':'9th grade',
+                '10':'10th grade',
+                '11':'11th grade',
+                '12':'12th grade'}
+            }
+demographics = [dictSex, dictRace, dictEthn, dictGrade]
 
-colList = list(respDict.values())
-rowList = []
-for qText in Qs: 
-    for val in list(dictSex['data'].values()): 
-        rowList.append(qText + " - " + val)
+# Query from raw data table 
+table = 'v_student_survey'
 
-# Query for post-course response by sex 
 for course in courses:
-    for qText in Qs: 
-        # Query data from the database for this section only
-        table = 'v_student_survey'
-        courseIDs = courses[course]
-        survey = 'Post-Course Student'
-        section = 'EN.500.109.01'
+    # Build LaTeX report header
+    latexFileName = "tex/"+course.replace(' ', '_')+".tex"
+    f = open(latexFileName, "w")
+    f.write("\documentclass{article}\n")
+    f.write(r"\usepackage[hidelinks]{hyperref}" + "\n")
+    f.write(r"\usepackage[top=1in, bottom=1in, left=1in, right=1in, "+
+            "marginparsep=0.15in]{geometry}")
+    f.write(r"\usepackage{booktabs}" + "\n")
+    f.write(r"\usepackage{graphicx}" + "\n")
+    f.write(r"\usepackage{caption}" + "\n")
+    f.write(r"\title{" + course + r" Summer 2021}")
+    f.write(r"\date{}")
+    #f.write(r"\author{"+teachers[section]+"}")
+    f.write(r"\begin{document} \maketitle"+"\n")
+    f.write(r"\setcounter{tocdepth}{1} \tableofcontents"+"\n")
+    f.write("\n" + r"\noindent ")
 
-        dfDem = pd.DataFrame(None,index=rowList,columns=colList)
-
-        dfDem.fillna(0, inplace=True)
-
-        for resp in respDict:
-            for r in dictSex['data']:
-                if resp == 'Total': 
-                    continue 
-                qstring = (
-                    "SELECT COUNT(v_student_survey.response)"
-                + " FROM " + table
-                + " INNER JOIN v_student_demographics ON v_student_survey.email_id=v_student_demographics.email_id"
-                + " WHERE v_student_survey.course_id IN " + courses[course]
-                + " AND v_student_survey.details = '" + survey + "'"
-                + " AND v_student_survey.question = '" + qText + "'"
-                + " AND v_student_survey.response = '" + resp + "'"
-                + " AND v_student_demographics." + dictSex['col'] + " = '" + r + "'"
-                )
-
-                nResp = queryForCount(qstring)
-                dfDem[respDict[resp]][qText + ' - ' + dictSex['data'][r]] = nResp
-                dfDem['Total'][qText + ' - ' + dictSex['data'][r]] += nResp
-            
-        for resp in respDict:
-            for r in dictSex['data']:
-                if resp == 'Total': 
-                    continue 
-                count = dfDem[respDict[resp]][qText + ' - ' + dictSex['data'][r]] 
-                total = dfDem['Total'][qText + ' - ' + dictSex['data'][r]] 
-                dfDem[respDict[resp]][qText + ' - ' + dictSex['data'][r]] = count / total 
-        dfDem.drop('Total', axis=1) # don't show total 
-            
-    print(dfDem)
-
+    # for each question 
+    for q in Qs: 
+        qText = Qs[q]
+        f.write(r"\section{"+qText+"}"+"\n")
     
-    #   Save
-    fname = course+'_general'
-    plt.savefig('tex/fig/'+fname+'.png', bbox_inches='tight', dpi=300)
-    plt.show()
+        for demo in demographics: 
+            colList = list(respDict.values())
+            rowList = []    
+            for var in demo['data']:
+                for survey in ['Pre-Course Student', 'Post-Course Student']: 
+                    rowList.append(survey + " - " + demo['data'][var])
+            # initialize raw values 
+            dfDem = pd.DataFrame(None,index=rowList,columns=colList)
+            dfDem.fillna(0, inplace=True)
+            # initialize percentage values 
+            dfPerc = pd.DataFrame(None,index=rowList,columns=colList)
+            dfPerc.drop('Total', axis=1) # don't show total 
+            dfPerc.fillna(0, inplace=True)
+
+            for survey in ['Pre-Course Student', 'Post-Course Student']: 
+                for var in demo['data']:
+                    total = 0
+                    for resp in respDict:
+                        if resp == 'Total': 
+                            continue 
+                        
+                        qstring = (
+                            "SELECT COUNT(v_student_survey.response)"
+                        + " FROM " + table
+                        + " INNER JOIN v_student_demographics ON v_student_survey.email_id=v_student_demographics.email_id"
+                        + " WHERE v_student_survey.course_id IN " + courses[course]
+                        + " AND v_student_survey.details = '" + survey + "'"
+                        + " AND v_student_survey.question = '" + q + "'"
+                        + " AND v_student_survey.response = '" + resp + "'"
+                        + " AND v_student_demographics." + demo['col'] + " = '" + var + "'"
+                        )
+                        nResp = queryForCount(qstring)
+                        dfDem[respDict[resp]][survey + ' - ' + demo['data'][var]] = nResp
+                        total += nResp
+                    # update dfPerc based on dfDem
+                    for resp in respDict:
+                        count = dfDem[respDict[resp]][survey + ' - ' + demo['data'][var]] 
+                        dfPerc[respDict[resp]][survey + ' - ' + demo['data'][var]] = count / total 
+            
+            # Plot data
+            #   Pre-formatting
+            font = {'weight':'normal', 'size':10}
+            plt.rc('font', **font)
+            fwidth = 6.5
+            fheight = 5
+            #   Plot and modify
+            ax = dfPerc.plot.barh(stacked=True, figsize=(fwidth, fheight))
+            ax.legend(loc='upper center', bbox_to_anchor=(0.2, -0.08),
+                        fancybox=False, shadow=False, ncol=5)
+            ax.spines[:].set_visible(False)
+            ax.yaxis.set_ticks_position('none')
+            ax.set_ylabel("")
+            
+            #   Save
+            fname = q+'___'+demo['col'].replace('_', ' ')
+            plt.savefig('tex/fig/'+fname+'.png', bbox_inches='tight', dpi=300)
+            f.write(r"\subsection{"+demo['col']+"}"+"\n")
+            f.write(r"\includegraphics[width="+str(fwidth)+"in]{fig/"+fname+r".png}\\"+"\n")
+
+    #%% WRAP-UP REPORT ========================================================
+    f.write("\end{document}")
+    f.close()
+    os.system("pdflatex -output-directory=tex -interaction=batchmode "+latexFileName)
